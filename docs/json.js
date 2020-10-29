2c14ed26b9aac1c3f181542ea1d7bda8eb30c3fc
justletterh <63440442+justletterh@users.noreply.github.com>
2020-10-29 13:03:46 -0500

    h

--- docs/json.js
+++ docs/json.js
@@ -6,39 +6,4 @@
  * Version: 0.4.0 (2014/04/10)
  * Released under the MIT license
  */
-define(["text"], function(text) {
-    var CACHE_BUST_QUERY_PARAM = "bust",
-        CACHE_BUST_FLAG = "!bust",
-        jsonParse = "undefined" != typeof JSON && "function" == typeof JSON.parse ? JSON.parse : function(val) {
-            return eval("(" + val + ")")
-        },
-        buildMap = {};
-
-    function cacheBust(n) {
-        return n = n.replace(CACHE_BUST_FLAG, ""), (n += n.indexOf("?") < 0 ? "?" : "&") + CACHE_BUST_QUERY_PARAM + "=" + Math.round(2147483647 * Math.random())
-    }
-    return {
-        load: function(n, e, t, i) {
-            i.isBuild && (!1 === i.inlineJSON || -1 !== n.indexOf(CACHE_BUST_QUERY_PARAM + "=")) || 0 === e.toUrl(n).indexOf("empty:") ? t(null) : text.get(e.toUrl(n), function(e) {
-                var r;
-                if (i.isBuild) buildMap[n] = e, t(e);
-                else {
-                    try {
-                        r = jsonParse(e)
-                    } catch (n) {
-                        t.error(n)
-                    }
-                    t(r)
-                }
-            }, t.error, {
-                accept: "application/json"
-            })
-        },
-        normalize: function(n, e) {
-            return -1 !== n.indexOf(CACHE_BUST_FLAG) && (n = cacheBust(n)), e(n)
-        },
-        write: function(n, e, t) {
-            e in buildMap && t('define("' + n + "!" + e + '", function(){ return ' + buildMap[e] + ";});\n")
-        }
-    }
-});
+define(["text"],function(text){var CACHE_BUST_QUERY_PARAM="bust",CACHE_BUST_FLAG="!bust",jsonParse="undefined"!=typeof JSON&&"function"==typeof JSON.parse?JSON.parse:function(val){return eval("("+val+")")},buildMap={};function cacheBust(n){return n=n.replace(CACHE_BUST_FLAG,""),(n+=n.indexOf("?")<0?"?":"&")+CACHE_BUST_QUERY_PARAM+"="+Math.round(2147483647*Math.random())}return{load:function(t,n,i,r){r.isBuild&&(!1===r.inlineJSON||-1!==t.indexOf(CACHE_BUST_QUERY_PARAM+"="))||0===n.toUrl(t).indexOf("empty:")?i(null):text.get(n.toUrl(t),function(n){var e;if(r.isBuild)buildMap[t]=n,i(n);else{try{e=jsonParse(n)}catch(n){i.error(n)}i(e)}},i.error,{accept:"application/json"})},normalize:function(n,e){return-1!==n.indexOf(CACHE_BUST_FLAG)&&(n=cacheBust(n)),e(n)},write:function(n,e,t){e in buildMap&&t('define("'+n+"!"+e+'", function(){ return '+buildMap[e]+";});\n")}}});
